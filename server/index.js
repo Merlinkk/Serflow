cb160a4c3ef4155a9f77ea00925c66b118f8c372
Merlinkk <anshulkashyap2504@gmail.com>
2024-04-04 17:40:00 +0530

    update

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const express = require("express")
+const app = require("./src/app")
+const PORT  = process.env.PORT || 5000
+const connectToMongo = require("./src/Middleware/ConnectToMongo")
+
+connectToMongo()
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  
+  
+  server.on('error', (error) => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  });
+
